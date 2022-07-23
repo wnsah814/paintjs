77a6749b8d3521a35fa8cc3534f32caad13f439c
성준모 <sjmskm@gmail.com>
2022-07-24 03:03:20 +0900

    #1-3 moveTo, lineTo

--- app.js
+++ app.js
@@ -3,5 +3,9 @@ const ctx = canvas.getContext("2d");
 canvas.width = 800;
 canvas.height = 800;
 
-ctx.rect(50, 50, 100, 100);
+ctx.moveTo(50, 50);
+ctx.lineTo(150, 50);
+ctx.lineTo(150, 150);
+ctx.lineTo(50, 150);
+ctx.lineTo(50, 50);
 ctx.fill();
