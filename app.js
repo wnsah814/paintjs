e3e1f81e6f8916c738a05d1011308588ad125173
성준모 <sjmskm@gmail.com>
2022-07-24 05:03:48 +0900

    #2-4 chage paint color

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
+const colorOptions = Array.from(document.getElementsByClassName("color-option"));
 const lineWidth = document.getElementById("line-width");
+const color = document.getElementById("color");
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
 canvas.width = 800;
@@ -28,9 +30,24 @@ function onLineWidthChange(event) {
     ctx.lineWidth = event.target.value;
 }
 
+function onColorChange(event) {
+    ctx.strokeStyle = event.target.value;
+    ctx.fillStyle = event.target.value;
+}
+
+function onColorClick(event) {
+    const colorValue = event.target.dataset.color;
+    ctx.strokeStyle = colorValue;
+    ctx.fillStyle = colorValue;
+    color.value = colorValue;
+}
+
 canvas.addEventListener("mousemove", onMove);
 canvas.addEventListener("mousedown", onMousedown);
 canvas.addEventListener("mouseup", cancelPainting);
 canvas.addEventListener("mouseleave", cancelPainting);
 
 lineWidth.addEventListener("change", onLineWidthChange);
+color.addEventListener("change", onColorChange);
+
+colorOptions.forEach(color => color.addEventListener("click", onColorClick));
