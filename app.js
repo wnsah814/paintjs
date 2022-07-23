5142265347c18392966dec9f72040a5b0d805ee4
성준모 <sjmskm@gmail.com>
2022-07-24 04:37:35 +0900

    #2-3 change line width

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
+const lineWidth = document.getElementById("line-width");
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
 canvas.width = 800;
 canvas.height = 800;
-ctx.lineWidth = 2;
+ctx.lineWidth = lineWidth.value;
 let isPainting = false;
 
 function onMove(event) {
     if (isPainting) {
         ctx.lineTo(event.offsetX, event.offsetY);
-        ctx.fill();
+        ctx.stroke();
         return;
     }
     ctx.moveTo(event.offsetX, event.offsetY);
@@ -20,9 +21,16 @@ function onMousedown() {
 
 function cancelPainting() {
     isPainting = false;
+    ctx.beginPath();
+}
+
+function onLineWidthChange(event) {
+    ctx.lineWidth = event.target.value;
 }
 
 canvas.addEventListener("mousemove", onMove);
 canvas.addEventListener("mousedown", onMousedown);
 canvas.addEventListener("mouseup", cancelPainting);
 canvas.addEventListener("mouseleave", cancelPainting);
+
+lineWidth.addEventListener("change", onLineWidthChange);
